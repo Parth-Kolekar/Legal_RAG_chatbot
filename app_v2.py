01d1ba84e45fece7b7e5e730621c8615eddad448
Parth-Kolekar <157197091+Parth-Kolekar@users.noreply.github.com>
2026-05-20 21:19:32 +0530

    fix 6

--- app_v2.py
+++ app_v2.py
@@ -4,8 +4,8 @@ import streamlit as st
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain.chains import ConversationalRetrievalChain
-from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
+# from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferWindowMemory
 from dotenv import load_dotenv
 
